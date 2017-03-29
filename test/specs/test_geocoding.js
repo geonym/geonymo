08b43dcd4631bc70ad42ea5f9c034a2af58ade99
flyusfly <fly_fly@popstar.com>
2017-03-29 17:32:23 +0200

    Tests > Check addresses, coordinates & geonym...

--- test/specs/test_geocoding.js
+++ test/specs/test_geocoding.js
@@ -2,32 +2,35 @@
 // File        : ./test/specs/test_geocoding.js
 // Desciption  : Geocoding tests
 // Date        : 2017.03.27 > Fly
-// --------------------------------------
+// --------------------------------------------
 
 var assert = require('assert')
 
 describe('webdriver.io page', function () {
-  // --------------------------------------------------------
-  // Test    : #01
-  // Action  : Address to be geocoded.
-  // Result  : Check returned Address & Coordinates & Geonym.
-  // --------------------------------------------------------
+  // -------------------------------------------------------------------
+  // Test: #01 - Check that Address & Geonym corresponds to coordinates.
+  // -------------------------------------------------------------------
   it('> Test > Gecoding > #01', function () {
     browser.url('http://localhost:3001')
 
+    // Insert data into the form
     var address = $('#data-address')
     address.setValue('139 Rue de Bercy 75012 Paris')
     browser.click('#button-geocoding')
     browser.pause(3000)
 
+    // Check map div
     var map = browser.getText('#map')
-    var mapAddress = map.search('139 Rue de Bercy 75012 Paris')
-    assert.notEqual(mapAddress, -1)
-    var userInfo = browser.getText('#userInfo')
-    var userInfoCoordinates = userInfo.search('[ 48.841025 ; 2.377966]')
-    assert.notEqual(userInfoCoordinates, -1)
-    var userInfoGeonym = userInfo.search('PP80-D2WN/1')
-    assert.notEqual(userInfoGeonym, -1)
-  })
+    var latitudeSolution = /48.841025/i
+    var longitudeSolution = /2.377966/i
+    var addressSolution = /139 Rue de Bercy 75012 Paris/i
+    assert.strictEqual(addressSolution.test(map), true)
 
+    // Check userInfo div
+    var userInfo = browser.getText('#userInfo')
+    assert.strictEqual(latitudeSolution.test(userInfo), true)
+    assert.strictEqual(longitudeSolution.test(userInfo), true)
+    var geonymSolution = /PP80-D2WN\/1/i
+    assert.strictEqual(geonymSolution.test(userInfo), true)
+  })
 })
